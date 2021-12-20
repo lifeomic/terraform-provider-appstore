ae52d79e8193c13afa0e2ae26e7aaf73d826c84a
Pearce Keesling <pearce.keesling@lifeomic.com>
2021-12-20 13:20:19 -0700

    Fix up example tf file

--- main.tf
+++ main.tf
@@ -1,3 +1,5 @@
+# Example of how to use the resource
+
 terraform {
   required_providers {
     appstore = {
@@ -13,7 +15,7 @@ variable "name" {
 
 variable "icons" {
   type = list(object({
-    src = string
+    src   = string
     sizes = string
   }))
 }
@@ -22,27 +24,27 @@ variable "start_url" {
   type = string
 }
 
-variable "description" { 
+variable "description" {
   type = string
 }
 
 # Not the right name, just for testing
 variable "url_base" {
-  type = string
+  type    = string
   default = "https://lifeapplets.dev.lifeomic.com" # The anxiety part should probably be separate
 }
 
 locals {
-    app_url = "${var.url_base}/anxiety"
-  }
+  app_url = "${var.url_base}/anxiety"
+}
 
 provider "appstore" {}
 
 resource "applet" "anxiety" {
-  provider = appstore
-  name = var.name
-  description = var.description
+  provider       = appstore
+  name           = var.name
+  description    = var.description
   author_display = "LifeOmic"
-  url = "${local.app_url}/${var.start_url}"
-  image = "${local.app_url}/${var.icons[0].src}"
+  url            = "${local.app_url}/${var.start_url}"
+  image          = "${local.app_url}/${var.icons[0].src}"
 }
